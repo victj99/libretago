2499c0cbf87fd1ce22c98c91b66d7d781cc1b2e9
robertoalex2323 <betodokij15@gmail.com>
2025-10-27 18:37:21 -0500

    Add role descriptions to RolesEnum class

--- src/main/java/com/utp/libretago/classes/RolesEnum.java
+++ src/main/java/com/utp/libretago/classes/RolesEnum.java
@@ -1,8 +1,12 @@
 package com.utp.libretago.classes;
 
 public class RolesEnum {
+    /** Rol de administrador del sistema. */
     public static final String ADMIN = "ADMIN";
+    /** Rol asignado a un colegio o institución educativa. */
     public static final String COLEGIO = "COLEGIO";
+    /** Rol asignado a un profesor dentro del sistema. */
     public static final String PROFESOR = "PROFESOR";
+    /** Rol asignado a un apoderado o tutor de un estudiante. */
     public static final String APODERADO = "APODERADO";
 }
